0522be73805d4ba31fd9d765e70eb8c28c32fa86
DavieChioa_glb <DavieChioa_glb@qq.com>
2019-09-06 20:22:15 +0800

    hot_fix

--- src/cn/tedu/test/Test.java
+++ src/cn/tedu/test/Test.java
@@ -3,5 +3,6 @@ package cn.tedu.test;
 public class Test {
 	public static void main(String[] args) {
     System.out.println(Runtime.getRuntime().maxMemory()/1024/1024);
+    System.out.println("hello");
 	}
 }
